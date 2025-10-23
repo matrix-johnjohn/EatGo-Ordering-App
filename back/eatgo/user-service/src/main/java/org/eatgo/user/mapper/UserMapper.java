f0379b22a83bf1eaf423c0fcf644b89956e1e781
matrix-johnjohn <1596903229@qq.com>
2025-10-23 16:27:36 +0800

    提交添加用户mapper

--- back/eatgo/user-service/src/main/java/org/eatgo/user/mapper/UserMapper.java
+++ back/eatgo/user-service/src/main/java/org/eatgo/user/mapper/UserMapper.java
@@ -1,8 +1,12 @@
 package org.eatgo.user.mapper;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.eatgo.common.domain.dto.LoginDto;
 
 @Mapper
 public interface UserMapper {
 
+    @Insert("insert into user (username,password,email) values (#{username},#{password},#{email})")
+    public void insertUser(LoginDto user);
 }
