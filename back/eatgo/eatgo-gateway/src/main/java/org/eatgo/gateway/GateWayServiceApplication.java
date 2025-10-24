20d91ed6b19e01b2515fa3b55d82d76230573805
matrix-johnjohn <1596903229@qq.com>
2025-10-24 20:26:46 +0800

    搭建网关微服务模块

---
+++ back/eatgo/eatgo-gateway/src/main/java/org/eatgo/gateway/GateWayServiceApplication.java
@@ -0,0 +1,11 @@
+package org.eatgo.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateWayServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayServiceApplication.class, args);
+    }
+}
